ea8719dabea31dcbe419704fe78256a32cdfa711
LaughingLemon <laughing.programmer@outlook.com>
2020-02-11 22:22:04 +0000

    Initialise

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using HoneyBear.HalClient;
+using HoneyBear.HalClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HalClient halClient = new HalClient();
+            halClient.HttpClient.BaseAddress = new Uri("http://localhost:8080");
+            IHalClient client = halClient.Root("/users").Get("users");
+            IEnumerable<User> products = client.Items<User>().Data();
+            foreach (User u in products)
+            {
+                Console.WriteLine("Name: " + u.Name + "");
+            }
+
+            var user = new { name = "Ian" };
+            halClient.Root("/").Post("users", user);
+        }
+    }
+
+    internal class User
+    {
+        public int Id { get; set;  }
+        public string Name { get; set;  }
+    }
+}
---
+++ wsdo/BaseWSDORepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.wsdo
+{
+    class BaseWSDORepository<T>
+    {
+        public void Delete(T t)
+        {
+
+        }
+
+        public T Update(T t)
+        {
+            return t;
+        }
+
+        public T Create(T t)
+        {
+            return t;
+        }
+
+        public T Get(int id)
+        {
+            return default(T);
+        }
+
+        public T[] Search(Object t)
+        {
+            return default(T[]);
+        }
+    }
+}
